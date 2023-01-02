43e3845ace2bbf737226c4ecf41c65610001e6de
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-02 17:07:55 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- fonts.mk
+++ fonts.mk
@@ -1,23 +1,3 @@
-# Copyright (C) 2018 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
-# PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
-# get installed too.
-
-PRODUCT_PACKAGES := \
-    Arvo-Regular.ttf \
-    Arvo-Italic.ttf \
-    Arvo-Bold.ttf \
-    Arvo-BoldItalic.ttf
+version https://git-lfs.github.com/spec/v1
+oid sha256:8525a7b35f3c4deabab84be6eec2e3796b7416e336c2bea8a3dfa05c1c046052
+size 886
